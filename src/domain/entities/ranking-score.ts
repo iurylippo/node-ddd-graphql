0577aeba1f8adef099e506c385d367293d2692ee
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-21 11:11:08 -0300

    first commit

---
+++ src/domain/entities/ranking-score.ts
@@ -0,0 +1,17 @@
+export type RankingScore = {
+  player: Player;
+  score: number;
+  matchDate: Date;
+  heroes: Hero[];
+}
+
+type Player = {
+  name: string;
+}
+
+type Hero = {
+  name: string;
+  level: number;
+}
+
+const t = ''
