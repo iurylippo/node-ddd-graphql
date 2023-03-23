c8a17b0ca630679d7635d561bf5522d0d851ca69
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-22 22:13:01 -0300

    feat: add cors on headers

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -8,6 +8,13 @@ const app = Fastify({
 })
 
 const bootstrap = async () => {
+  app.addHook('preHandler', (req, res, done) => {
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST,GET,HEAD,PUT,PATCH,DELETE,OPTIONS')
+    done()
+  })
+
   await setupApolloServer(app)
   await setupRoutes(app)
 
